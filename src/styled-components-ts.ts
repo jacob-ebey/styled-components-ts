c94374d898d514a970a961635aa253f406d1c627
Jacob Ebey <jacob.ebey@live.com>
2018-04-06 16:25:31 -0700

    test

--- src/styled-components-ts.ts
+++ src/styled-components-ts.ts
@@ -4,7 +4,7 @@ import { ThemedStyledFunction, StyledComponentClass } from 'styled-components'
 const styledComponentWithProps =
   <SProps, TProps extends object = {}, U extends HTMLElement = HTMLElement>
     (
-    styledFunction: ThemedStyledFunction<any, any>, // tslint:disable-line
+    styledFunction: ThemedStyledFunction<any, any>, // tslint:disable-line no-any
   ): ThemedStyledFunction<SProps & React.HTMLProps<U>, TProps> => {
     return styledFunction
   }
